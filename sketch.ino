74219ea4d9dd50dd8c0951e6279272d0697b5a8c
github host <110337548+github-host2@users.noreply.github.com>
2023-01-25 13:41:23 +0100

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,35 +1,19 @@
 int value = 0;
 
-void setup()
-{
+void setup(){
   pinMode (13, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop()
-{
-sensorWert =analogRead(eingang); //Die Spannung an dem Fotowiderstand auslesen und unter der Variable „sensorWert“ abspeichern.
-Serial.print("Sensorwert = " ); //Ausgabe am Serial-Monitor: Das Wort „Sensorwert: „
-Serial.println(sensorWert); //Ausgabe am Serial-Monitor. Mit dem Befehl Serial.print wird der Sensorwert des Fotowiderstandes in Form einer Zahl zwischen 0 und 1023 an den serial monitor gesendet.
-
-if (sensorWert > 512 ) //Wenn der Sensorwert über 512 beträgt….
-{
-digitalWrite(LED, HIGH); //…soll die LED leuchten…
+void loop(){
+value = analogRead(A0);
+Serial.print("Value = ");
+Serial.println(value);
+if (value > 512 ){
+  digitalWrite(13, 1);
 }
-
-else //andernfalls…
-{
-digitalWrite(LED, LOW); //….soll sie nicht leuchten.
+else {
+  digitalWrite(13, 0);
 }
-
-delay (50);//Eine kurze Pause, in der die LED an oder aus ist
-
-}//Mit dieser letzten Klammer wird der Loop-Teil geschlossen.
-
-//Wenn nun der Sensorwert bei normaler Helligkeit bspw. nur den Wert 100 hat 
-//(Der Wert ist abhängig von den verwendeten Widerständen, von der 
-//Helligkeit und von der Stromrichtung), dann nimmt man anstelle des Wertes 
-//512 einen wesentlich kleineren Wert, bei dem die LED zu leuchten beginnen
-//soll. Bspw. nimmt man dann den Wert 90. Den aktuellen Sensorwert kann 
-//man sich nun mit Hilfe des „Serial monitor“ anzeigen lassen. Dazu klickt man 
-//oben auf „Tools“ und anschließend auf „serial monitor“. 
+delay(50);
+}
